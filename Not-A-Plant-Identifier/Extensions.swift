d255c4339ba662dbac7832918dfa2d19161291c4
AGonzalez5195 <AnthonyGonzalez@pursuit.org>
2019-09-17 12:02:07 -0400

    Added the app icon and the pokeball button now shakes when pressed, courtesy of Tom.

--- Not-A-Plant-Identifier/Extensions.swift
+++ Not-A-Plant-Identifier/Extensions.swift
@@ -7,9 +7,32 @@
 //
 
 import Foundation
+import UIKit
+
 
 extension Sequence {
     var joinedStringFromArray: String {
         return map { "\($0)" }.joined(separator: ", ")
     }
+}
+
+extension UIButton {
+    func shake() {
+        let shake = CABasicAnimation(keyPath: "position")
+        shake.duration = 0.1
+        shake.repeatCount = 2
+        shake.autoreverses = true
+        
+        let fromPoint = CGPoint(x: center.x - 10, y: center.y)
+        let fromValue = NSValue(cgPoint: fromPoint)
+        
+        let toPoint = CGPoint(x: center.x + 10, y: center.y)
+        let toValue = NSValue(cgPoint: toPoint)
+        
+        shake.fromValue = fromValue
+        shake.toValue = toValue
+        
+        layer.add(shake, forKey: nil)
+    }
+    
 }
---
+++ Not-A-Plant-Identifier/titleScreenViewController.swift
@@ -0,0 +1,25 @@
+//
+//  titleScreenViewController.swift
+//  Not-A-Plant-Identifier
+//
+//  Created by Anthony Gonzalez on 9/17/19.
+//  Copyright © 2019 The Bootlegged Pokémon Company. All rights reserved.
+//
+
+import UIKit
+
+class titleScreenViewController: UIViewController {
+
+    @IBAction func pokeballButton(_ sender: UIButton) {
+        sender.shake()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.8) { [unowned self] in
+            self.performSegue(withIdentifier: "pokeSegue", sender: nil)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
